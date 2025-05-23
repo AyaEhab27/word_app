6955d392e879095396cf205d82ab8e628e167ab4
Aya Ehab <xxxeeee2468@gmail.com>
2025-05-23 14:02:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,24 @@ app = FastAPI()
 @app.post("/translate/")
 async def translate_text(text: str = Form(None), audio: UploadFile = File(None)):
     if not text and not audio:
-        return {"error": "يجب إدخال نص أو ملف صوتي"}
+        return {"error": "Enter text or voice"}
 
     if audio:
         audio_bytes = await audio.read()
         text = convert_audio_to_text(audio_bytes)
         if not text:
-            return {"error": "تعذر تحويل الصوت إلى نص"}
+            return {"error": "Error in text or voice"}
 
     language = detect_language(text)
     cleaned_text = clean_and_split_text(text, language)
     animation_urls = get_animation_links(cleaned_text, language)
     
-    # حفظ الروابط في Cloudinary مع الحفاظ على نفس الاسم
+    
     cloudinary_urls = []
     for url in animation_urls:
         try:
-            # استخراج اسم الملف من الرابط
-            filename = os.path.basename(url).split('?')[0]  # إزالة أي query parameters
+            
+            filename = os.path.basename(url).split('?')[0]  
             cloudinary_url = upload_to_cloudinary(url, public_id=filename)
             cloudinary_urls.append(cloudinary_url)
         except Exception as e:
